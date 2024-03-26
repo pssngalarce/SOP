e3bc0bb55167a412eee94965e173ffd16fdfa44c
pssngalarce <165096750+pssngalarce@users.noreply.github.com>
2024-03-26 19:41:09 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='SOP',
-    version='0.1',
-    packages=['SOP'],
-   )
+    name='SOP',  # Nombre del paquete
+    version='0.1',  # Versión del paquete
+    packages=find_packages(),  # Encuentra automáticamente los paquetes del proyecto
+    install_requires=[  # Lista de dependencias necesarias para el paquete
+        'numpy',
+        'matplotlib'
+    ],
+    author='Tu Nombre',  # Tu nombre
+    author_email='tu@email.com',  # Tu correo electrónico
+    description='Una descripción corta de tu paquete',  # Descripción breve del paquete
+    url='https://github.com/pssngalarce/SOP.git',  # URL del repositorio del proyecto
+)
